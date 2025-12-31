e6f596e6be0f8e73c2d6795fb5b9642e937541b8
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2025-12-31 20:14:08 +0530

    new commit

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,12 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import Actual from './Props/Top-Down/Actual'
+// import Actual from './Props/Bottom-Up/Actual'
+// import Actual from './Props/Children/Actual'
 
-function App() {
+// import CheckContext from './Props/Context API/CheckContext'
+
+// import TailwindCss from './TailWind CSS/TailwindCss'
+
+// import If from './Conditional/If/If'
+// import Ternary from "./Conditional/Ternary/Ternary"
+
+// import Map from "./Array/Method/Map"
+
+// import InlineCSS from "./CSS/InlineCSS"
+// import File from "./CSS/CSS Stylesheet/File"
+import File from './CSS/CSS Module/File'
+
+
+function App() {  
 
   return (
     <>
+
+    <h1>App</h1>
+
+
+    {/* <Actual /> */}
+
+    {/* <CheckContext /> */}
+
+
+    {/* <TailwindCss /> */}
+
+
+    {/* <If value={false}/> */}
+
+    {/* <Ternary value={true} /> */}
+
+
+    {/* <Map /> */}
+
+
+    {/* <InlineCSS /> */}
+
+    {/* <File /> */}
+
+    {/* <File /> */}
+
+
+
+
+    
+
+
      
     </>
   )
--- React/src/main.jsx
+++ React/src/main.jsx
@@ -1,10 +1,14 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './App.jsx'
 
+import Context from './Props/Context API/Context.jsx'
+
+
 createRoot(document.getElementById('root')).render(
-     <Context >
+  
+     <Context>
 
           <App />
           
