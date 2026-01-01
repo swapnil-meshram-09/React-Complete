876649165f45e27f8699cb971e504da4330885f2
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-01 17:12:41 +0530

    new commit

---
+++ React/src/Array/Map.jsx
@@ -0,0 +1,32 @@
+// function Map(){
+
+//     const Cars = ['BMW', 'Audi', 'Mustang']
+
+//     const func = () =>{
+//         const result = Cars.map((car) =>{
+//             {car}
+//         })
+//         return result
+//     }
+
+//     const func2 = () =>{
+//         console.log('hello');
+        
+//     }
+
+//     return(
+//         <>
+
+//        {/* {Cars}  */}
+       
+//        {/* {Cars[1]} */}
+
+//        <button onClick={func}>Click</button>
+
+
+//         </>
+//     )
+// }
+
+// export default Map
+
