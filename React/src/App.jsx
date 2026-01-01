7243ad91d209bd87b0fd1dae46539888fc125641
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-01 17:18:01 +0530

    new commit

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,18 +1,19 @@
-// import Actual from './Props/Top-Down/Actual'
-// import Actual from './Props/Bottom-Up/Actual'
-// import Actual from './Props/Children/Actual'
+import Actual from './Props/Top-Down/Actual'
+import Actual from './Props/Bottom-Up/Actual'
+import Actual from './Props/Children/Actual'
 
-// import CheckContext from './Props/Context API/CheckContext'
+import CheckContext from './Props/Context API/CheckContext'
 
-// import TailwindCss from './TailWind CSS/TailwindCss'
+import TailwindCss from './TailWind CSS/TailwindCss'
 
-// import If from './Conditional/If/If'
-// import Ternary from "./Conditional/Ternary/Ternary"
+import If from './Conditional/If/If'
+import Ternary from "./Conditional/Ternary/Ternary"
 
-// import Map from "./Array/Method/Map"
+import Map from "./Array/Method/Map"
 
-// import InlineCSS from "./CSS/InlineCSS"
-// import File from "./CSS/CSS Stylesheet/File"
+import InlineCSS from "./CSS/InlineCSS"
+import File from "./CSS/CSS Stylesheet/File"
+
 import File from './CSS/CSS Module/File'
 
  
