949b083afcf4d585c2a85a3d9b3c4a004b9e9338
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-02 16:17:10 +0530

    new commit

---
+++ React/src/form/Form.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+function Form(){
+
+    const [title, setTitle] = useState('')
+
+    return(
+
+        <>
+        <form action="" onSubmit={(e)=>{
+            e.preventDefault()
+            setTitle('')
+        }}>
+
+            <input type="text" placeholder="enter" value={title} onChange={(e)=>{
+                setTitle(e.target.value)
+            }} />
+            <br /><br />
+            <button type='submit'>Submit</button>
+        </form>
+        
+        </>
+    )
+}
+
+export default Form
