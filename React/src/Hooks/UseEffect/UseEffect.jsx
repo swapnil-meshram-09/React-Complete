2edc9cea985bbc850dc01502634b5541d9a54937
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-05 21:15:05 +0530

    new commit

---
+++ React/src/Hooks/UseEffect/UseEffect.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react"
+
+function UseEffect() {
+
+//   useEffect(()=> {
+
+//     console.log('hello');         // auto executed 1 time
+//   }, [])
+
+  return (
+    <>
+     <h5>{value}</h5>
+    </>
+  )
+}
+
+export default UseEffect
